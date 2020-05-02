dca5bc53af60233a1b00bb26135c020835099a06
dqq0019 <61773666+dqq0019@users.noreply.github.com>
2020-05-02 16:01:56 +0800

    Add files via upload

---
+++ print.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void printBoard(int ***board, int SIZE){
+  for (int i=0;i<SIZE;i++){
+    cout<<"Level "<<i+1<<' ';
+  }
+  cout<<endl;
+  for (int j=0;j<SIZE;j++){
+    for (int i=0;i<SIZE;i++){
+      for (int k=0;k<SIZE;k++){
+        cout<<board[i][j][k]<<' ';
+      }
+      cout<<'	';
+    }
+    cout<<endl;
+  }
+}
